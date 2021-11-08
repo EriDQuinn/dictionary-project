af73d1533fc50464df001f28035d5617177898dc
ErikaDiaz <edaeri@live.com.mx>
2021-11-07 22:49:07 -0600

    adding missing code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img
           src="https://tse1.mm.bing.net/th?id=OIP.ufRBJfaOH9RPQ6cmXzFTpgHaHa&pid=Api&P=0&w=300&h=300"
           className="App-image"
-          alt="image"
+          alt="peonies"
         />
       </header>
     </div>
