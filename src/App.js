257ac80123a61dbc28e7d2e7a78cd4b80fccf911
ErikaDiaz <edaeri@live.com.mx>
2021-11-08 01:16:01 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="https://tse1.mm.bing.net/th?id=OIP.ufRBJfaOH9RPQ6cmXzFTpgHaHa&pid=Api&P=0&w=300&h=300"
-          className="App-image"
-          alt="peonies"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src="https://tse1.mm.bing.net/th?id=OIP.ufRBJfaOH9RPQ6cmXzFTpgHaHa&pid=Api&P=0&w=300&h=300"
+            className="App-image"
+            alt="peonies"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Erika Díaz</footer>
+      </div>
     </div>
   );
 }
+
+export default App;
