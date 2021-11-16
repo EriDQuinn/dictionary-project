fadd13f50aa8f91d2024f05b04a2ae096ccc9cf7
ErikaDiaz <edaeri@live.com.mx>
2021-11-15 23:22:52 -0600

    Adding new component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,11 +9,15 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <strong>Definition:</strong>
+
+            <p> {definition.definition}</p>
             <hr />
+            <strong>Example:</strong>
             <em>
               <p>{definition.example}</p>
             </em>
+            <Synonyms synonyms={definition.synonyms}></Synonyms>
           </div>
         );
       })}
